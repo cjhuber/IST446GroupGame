2cdb860fa89cfcf095579d441d6b852ee80867f4
Dylan Nguyen <d_nguyen@live.com>
2015-04-15 18:35:32 -0400

    Start parsing map from server

--- dungeon-crawl/Assets/Scripts/LevelParser.cs
+++ dungeon-crawl/Assets/Scripts/LevelParser.cs
@@ -6,8 +6,8 @@ using SimpleJSON;
 public class LevelParser : MonoBehaviour
 {
 
-	public Transform wall;
-	public Transform floor;
+	public GameObject wall;
+	public GameObject floor;
 
 
 
@@ -17,15 +17,28 @@ public class LevelParser : MonoBehaviour
 	void Start ()
 	{
 	    string temp = mapData.text;
-//		RetrieveMapFromServer();
-        ParseMap(temp);
+		StartCoroutine(RetrieveMapFromServer());
 	}
 
-	private void RetrieveMapFromServer()
+	IEnumerator RetrieveMapFromServer()
 	{
-		WWW www = new WWW("http://localhost:8000/output.txt");
+		// Use POST request for testing
+		WWWForm form = new WWWForm();
+		form.AddField ("player_1", "123");
+		form.AddField ("player_2", "456");
 
-		mapFromServer = www.text;
+		WWW www = new WWW("http://localhost:3000/rooms", form);
+		yield return www;
+		if (www.error == null) {
+			Debug.Log ("Request ok");
+			Debug.Log (www.text);
+			mapFromServer = www.text;
+			ParseMap(mapFromServer);
+		} else {
+			Debug.Log ("Request error" + www.error);
+		}
+		//mapFromServer = www.text;
+		//Debug.Log(mapFromServer);
 	}
 
 	// Update is called once per frame
@@ -34,40 +47,66 @@ public class LevelParser : MonoBehaviour
 	}
 
     void ParseMap(String map)
-    {
-        String parsedMap = map.Replace("],[", "$");
-        parsedMap = parsedMap.Replace("[[", "");
-        parsedMap = parsedMap.Replace("]]", "");
-        char[] delimiter = {'$'};
-
-        string[] seperatedParsedMap = parsedMap.Split(delimiter);
-        Debug.Log("Map Height,  " + seperatedParsedMap.Length);
+	{
+		var tileWidth = wall.GetComponent<SpriteRenderer>().bounds.size.x;
+		var tileHeight = wall.GetComponent<SpriteRenderer>().bounds.size.y;
+		Debug.Log ("Parsing map");
+        var room = JSONNode.Parse (mapFromServer);
+		Debug.Log ("Done parsing map");
+		Debug.Log (room["map"]["properties"].ToString());
+		int mapWidth = room["map"]["properties"]["width"].AsInt;
+		int mapHeight = room["map"]["properties"]["width"].AsInt;
+		Debug.Log ("Map Width:" + mapWidth);
+		Debug.Log ("Map Height:" + mapHeight);
 		Camera c = GetComponent<Camera>();
-		float y = c.pixelHeight;
+		float y2 = c.pixelHeight;
 		Vector3 startingPoint = c.ScreenToWorldPoint(new Vector3(0, c.pixelHeight, 5));
 		Vector3 tileUnit = c.ScreenToWorldPoint(new Vector3(16, 16, 5));
-        foreach (string s in seperatedParsedMap)
-        {
-            string temp = (s.Replace("\",\"", "")).Trim(new char[] {' ', '"', ','});
-			float x = 0;
-            foreach(char z in temp)
-			{
-				switch (z)
-				{
-				case '1':
-					Instantiate(wall, c.ScreenToWorldPoint(new Vector3(x, y, 5)), Quaternion.identity);
-					break;
-				case '0':
-					Instantiate(floor, c.ScreenToWorldPoint(new Vector3(x, y, 5)), Quaternion.identity);
-					break;
-				default:
-					break;
-				}
-				x += 16;
-			}
 
-			y -= 16;
-            Debug.Log(temp);  
-        }
+		for (int x = 0; x < mapWidth; x++) {
+			for (int y = 0; y < mapHeight; y++) {
+				int type = room["map"]["terrain"][x][y].AsInt;
+				if (type == 0) {
+					Instantiate (floor, c.ScreenToWorldPoint (new Vector3(x * tileWidth, y * tileHeight, 0)), Quaternion.identity);
+				} else if (type == 1) {
+					
+					Instantiate (wall, c.ScreenToWorldPoint (new Vector3(x * tileWidth, y * tileHeight, 0)), Quaternion.identity);
+				}
+			}
+		}
+//        String parsedMap = map.Replace("],[", "$");
+//        parsedMap = parsedMap.Replace("[[", "");
+//        parsedMap = parsedMap.Replace("]]", "");
+//        char[] delimiter = {'$'};
+//
+//        string[] seperatedParsedMap = parsedMap.Split(delimiter);
+//        Debug.Log("Map Height,  " + seperatedParsedMap.Length);
+//		Camera c = GetComponent<Camera>();
+//		float y = c.pixelHeight;
+//		Vector3 startingPoint = c.ScreenToWorldPoint(new Vector3(0, c.pixelHeight, 5));
+//		Vector3 tileUnit = c.ScreenToWorldPoint(new Vector3(16, 16, 5));
+//        foreach (string s in seperatedParsedMap)
+//        {
+//            string temp = (s.Replace("\",\"", "")).Trim(new char[] {' ', '"', ','});
+//			float x = 0;
+//            foreach(char z in temp)
+//			{
+//				switch (z)
+//				{
+//				case '1':
+//					Instantiate(wall, c.ScreenToWorldPoint(new Vector3(x, y, 5)), Quaternion.identity);
+//					break;
+//				case '0':
+//					Instantiate(floor, c.ScreenToWorldPoint(new Vector3(x, y, 5)), Quaternion.identity);
+//					break;
+//				default:
+//					break;
+//				}
+//				x += 16;
+//			}
+//
+//			y -= 16;
+//            Debug.Log(temp);  
+//        }
     }
 }
