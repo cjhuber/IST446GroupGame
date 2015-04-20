7ac2990bc24a843a7b9628d2f41ddc9656c0fe68
Dylan Nguyen <d_nguyen@live.com>
2015-04-20 12:52:07 -0400

    Faster map parsing

--- dungeon-crawl/Assets/Scripts/LevelParser.cs
+++ dungeon-crawl/Assets/Scripts/LevelParser.cs
@@ -58,55 +58,17 @@ public class LevelParser : MonoBehaviour
 		int mapHeight = room["map"]["properties"]["width"].AsInt;
 		Debug.Log ("Map Width:" + mapWidth);
 		Debug.Log ("Map Height:" + mapHeight);
-		Camera c = GetComponent<Camera>();
-		float y2 = c.pixelHeight;
-		Vector3 startingPoint = c.ScreenToWorldPoint(new Vector3(0, c.pixelHeight, 5));
-		Vector3 tileUnit = c.ScreenToWorldPoint(new Vector3(16, 16, 5));
 
 		for (int x = 0; x < mapWidth; x++) {
 			for (int y = 0; y < mapHeight; y++) {
 				int type = room["map"]["terrain"][x][y].AsInt;
 				if (type == 0) {
-					Instantiate (floor, c.ScreenToWorldPoint (new Vector3(x * tileWidth, y * tileHeight, 0)), Quaternion.identity);
+					Instantiate (floor, (new Vector3(x * tileWidth, y * tileHeight, 0)), Quaternion.identity);
 				} else if (type == 1) {
 					
-					Instantiate (wall, c.ScreenToWorldPoint (new Vector3(x * tileWidth, y * tileHeight, 0)), Quaternion.identity);
+					Instantiate (wall, (new Vector3(x * tileWidth, y * tileHeight, 0)), Quaternion.identity);
 				}
 			}
 		}
-//        String parsedMap = map.Replace("],[", "$");
-//        parsedMap = parsedMap.Replace("[[", "");
-//        parsedMap = parsedMap.Replace("]]", "");
-//        char[] delimiter = {'$'};
-//
-//        string[] seperatedParsedMap = parsedMap.Split(delimiter);
-//        Debug.Log("Map Height,  " + seperatedParsedMap.Length);
-//		Camera c = GetComponent<Camera>();
-//		float y = c.pixelHeight;
-//		Vector3 startingPoint = c.ScreenToWorldPoint(new Vector3(0, c.pixelHeight, 5));
-//		Vector3 tileUnit = c.ScreenToWorldPoint(new Vector3(16, 16, 5));
-//        foreach (string s in seperatedParsedMap)
-//        {
-//            string temp = (s.Replace("\",\"", "")).Trim(new char[] {' ', '"', ','});
-//			float x = 0;
-//            foreach(char z in temp)
-//			{
-//				switch (z)
-//				{
-//				case '1':
-//					Instantiate(wall, c.ScreenToWorldPoint(new Vector3(x, y, 5)), Quaternion.identity);
-//					break;
-//				case '0':
-//					Instantiate(floor, c.ScreenToWorldPoint(new Vector3(x, y, 5)), Quaternion.identity);
-//					break;
-//				default:
-//					break;
-//				}
-//				x += 16;
-//			}
-//
-//			y -= 16;
-//            Debug.Log(temp);  
-//        }
     }
 }
