9a377585735e9b7ad2d930c325e3e4483f4a1082
Dylan Nguyen <d_nguyen@live.com>
2015-04-21 12:03:13 -0400

    Add temporary camera movement script - WASD/arrow keys to move, z to zoom in, x to zoom out

---
+++ dungeon-crawl/Assets/Scripts/CameraDebug.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDebug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		//Vector3 move = new Vector3(horizontal, vertical, Camera.main.transform.position.z);
+		Debug.Log ("movement: " + horizontal + "," + vertical);
+		this.transform.position += new Vector3(horizontal, vertical, 0);
+
+		if (Input.GetKey(KeyCode.Z)) {
+			this.GetComponent<Camera>().fieldOfView += 5;
+		} else if (Input.GetKey(KeyCode.X)) {
+			this.GetComponent<Camera>().fieldOfView -= 5;
+		}
+	}
+}
