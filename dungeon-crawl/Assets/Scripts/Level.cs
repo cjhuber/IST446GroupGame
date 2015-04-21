a81ce1e4643349e34dac942a73d20ce02ea20755
Dylan Nguyen <d_nguyen@live.com>
2015-04-21 11:33:53 -0400

    Add temporary player prefab Add parsing for player spawn point Add an empty Level game object to keep track of level state.

--- dungeon-crawl/Assets/Scripts/Level.cs
+++ dungeon-crawl/Assets/Scripts/Level.cs
@@ -3,73 +3,71 @@ using UnityEngine;
 using System.Collections;
 using SimpleJSON;
 
-public class LevelParser : MonoBehaviour
-{
+public class Level : MonoBehaviour {
 
 	public GameObject wall;
-	public GameObject floor;
+	public GameObject ground;
+	public GameObject player;
+	private String rawMapData;
+	public Vector3 playerSpawn;
 
-
-
-    public TextAsset mapData;
 	// Use this for initialization
-	private string mapFromServer;
-	void Start ()
-	{
-	    string temp = mapData.text;
-		StartCoroutine(RetrieveMapFromServer());
+	void Start () {
+		StartCoroutine(GetMap());
 	}
-
-	IEnumerator RetrieveMapFromServer()
-	{
-		// Use POST request for testing
-		WWWForm form = new WWWForm();
-		form.AddField ("player_1", "123");
-		form.AddField ("player_2", "456");
-
-		WWW www = new WWW("http://localhost:3000/rooms", form);
-		yield return www;
-		if (www.error == null) {
-			Debug.Log ("Request ok");
-			Debug.Log (www.text);
-			mapFromServer = www.text;
-			ParseMap(mapFromServer);
-		} else {
-			Debug.Log ("Request error" + www.error);
-		}
-		//mapFromServer = www.text;
-		//Debug.Log(mapFromServer);
-	}
-
+	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
-    void ParseMap(String map)
-	{
+	private IEnumerator GetMap() {
+		// Use POST request for testing
+		WWWForm form = new WWWForm();
+		form.AddField ("player_1", "123");
+		form.AddField ("player_2", "456");
+		
+		WWW www = new WWW("http://localhost:3000/rooms", form);
+		// Wait for request to finish
+		yield return www;
+		if (www.error == null) {
+			Debug.Log ("Request ok");
+			Debug.Log (www.text);
+			rawMapData = www.text;
+			ParseMapData();
+		} else {
+			Debug.Log ("Request error" + www.error);
+		}
+	}
+
+	private void ParseMapData() {
+		
+		Debug.Log ("Parsing map");
+
 		var tileWidth = wall.GetComponent<SpriteRenderer>().bounds.size.x;
 		var tileHeight = wall.GetComponent<SpriteRenderer>().bounds.size.y;
-		Debug.Log ("Parsing map");
-        var room = JSONNode.Parse (mapFromServer);
-		Debug.Log ("Done parsing map");
-		Debug.Log (room["map"]["properties"].ToString());
+		var room = JSONNode.Parse (rawMapData);
 		int mapWidth = room["map"]["properties"]["width"].AsInt;
 		int mapHeight = room["map"]["properties"]["width"].AsInt;
 		Debug.Log ("Map Width:" + mapWidth);
 		Debug.Log ("Map Height:" + mapHeight);
 		Debug.Log ("Tile size: " + tileWidth + "x" + tileHeight);
 
+		// Parse terrain/tiles
 		for (int x = 0; x < mapWidth; x++) {
 			for (int y = 0; y < mapHeight; y++) {
 				int type = room["map"]["terrain"][x][y].AsInt;
 				if (type == 0) {
-					Instantiate (floor, (new Vector3(x, y, 0)), Quaternion.identity);
+					Instantiate (ground, (new Vector3(x, y, 0)), Quaternion.identity);
 				} else if (type == 1) {
 					
 					Instantiate (wall, (new Vector3(x, y, 0)), Quaternion.identity);
 				}
 			}
 		}
-    }
+
+		playerSpawn = new Vector3(room["map"]["playerSpawn"]["x"].AsFloat, room["map"]["playerSpawn"]["y"].AsFloat, -2);
+		Debug.Log (playerSpawn);
+		Instantiate(player, playerSpawn, Quaternion.identity);
+	}
 }
